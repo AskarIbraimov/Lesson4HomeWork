f23e8b3614cb647be557c815a3d65e53777a5cf2
AskarIbraimov <raksalgz@gmail.com>
2021-05-07 16:03:44 +0600

    исправил небольшие ошибки, добавил героев

--- src/com/GeekTech/Main.java
+++ src/com/GeekTech/Main.java
@@ -3,15 +3,16 @@ package com.GeekTech;
 import java.util.Random;
 
 public class Main {
+    boolean evasion = false;
 
     public static String bossName = "Vilgefortz";
     public static int bossHP = 1000;
     public static int bossDamage = 33;
     public static String ultimate;
 
-    public static String[] heroesName = {"Gerald","Regis","Yennefer","Medic"};
-    public static int[] heroesHP = {380, 350, 400,600};
-    public static int[] heroesDamage = {25, 18, 21,0};
+    public static String[] heroesName = {"Gerald","Regis","Yennefer","Medic","Golem","Lucky"};
+    public static int[] heroesHP = {380, 350, 400,600,800,200};
+    public static int[] heroesDamage = {25, 18, 21,0,5,40};
     public static int roundNumber = 0;
     public static String medicUltimate = "Medic Heal: ";
 
@@ -57,6 +58,8 @@ public class Main {
         ultimate = criticalStrike();
         heroesDamage();
         printStatistics();
+        golem();
+        evasion();
 
     }
 
@@ -64,7 +67,11 @@ public class Main {
         for (int i = 0; i < heroesHP.length; i++) {
             if (heroesHP[i] > 0 && bossHP > 0) {heroesHP[i] = heroesHP[i] - bossDamage;}
 
+
+
+
         }
+
 
 
     }
@@ -72,11 +79,11 @@ public class Main {
     public static void heroesDamage(){
         Random random = new Random();
         int coeff = random.nextInt(3 ) +2;
-        for (int i = 0; i < heroesDamage.length - 1; i++) {
+        for (int i = 0; i < heroesDamage.length; i++) {
             if (heroesHP[i] > 0 && bossHP > 0) {
-                if (ultimate == heroesName[i]){
+                if (ultimate == heroesName[i] ){
                     bossHP = bossHP - (heroesDamage[i] * coeff);
-                    System.out.println(" Critical hit by " + " " + (heroesDamage[i] * coeff));
+                    System.out.println(" Critical hit by " + ultimate + " " + (heroesDamage[i] * coeff));
                 } else {
                 bossHP = bossHP - heroesDamage[i];
                 }
@@ -92,10 +99,13 @@ public class Main {
 
     }
 
-    public static String criticalStrike(){
+    public static String criticalStrike() {
         Random random = new Random();
         int randomIndex = random.nextInt(heroesName.length);
-        return heroesName[randomIndex];
+        if (randomIndex == 3) {
+            randomIndex = random.nextInt(heroesName.length);
+        }
+            return heroesName[randomIndex];
     }
 
     public static  void printStatistics(){
@@ -114,7 +124,7 @@ public class Main {
                 if (heroesHP[3] <=0 ) {
                     heroesHP[3] = 0;
                 }
-                if (heroesHP[i] < 100 && heroesHP[i] >= 0) {
+                else if (heroesHP[i] < 100 && heroesHP[i] >= 0) {
                     Random random = new Random();
                     int r= random.nextInt(50);
                     System.out.println("Medic healed "+heroesName[i] + " by "
@@ -125,6 +135,32 @@ public class Main {
             }
         }
     }
+
+   public static  void golem(){
+        if(heroesHP[4] > 0){
+            for (int i = 0; i < heroesHP.length ; i++) {
+                if (heroesHP[i] != heroesHP[4]){
+                    heroesHP[i] += bossDamage /5;
+                    heroesHP[4] -= bossDamage /5;
+                }
+                
+            }
+        }
+
+   }
+
+
+   public static  void evasion(){
+        Random random = new Random();
+        int randomEvasion = random.nextInt(2);
+        if (randomEvasion ==1) {
+            heroesHP[5] += bossDamage;
+            System.out.println(heroesName[5] + " Уклонился от " + bossName);
+        }
+   }
+
+
+
 
 
 
